1699a4f27f42e8bce556d00499e22f202b6016fa
greenh <greenh@gmail.com>
2025-01-30 21:22:17 +0200

    fixes

--- ec2.tf
+++ ec2.tf
@@ -1,50 +1,9 @@
-resource "aws_security_group" "test-ec2-sg" {
-  name        = "${var.project}-test-ec2-sg"
-  description = "Allow inbound SSH and HTTP"
+module "ec2" {
+  source = "./modules/ec2/"
+  instance_type = var.instance_type
+  public_key = var.public_key
+  ubuntu_ami = var.ubuntu_ami
+  project = var.project
+  subnet_id = aws_subnet.public-1.id
   vpc_id = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.project}-test-ec2-sg"
-  }
-}
-
-resource "aws_key_pair" "my_key" {
-  key_name   = "my-key-pair"
-  public_key = var.public_key  # Замените на ваш публичный ключ
-}
-
-resource "aws_instance" "my_ec2" {
-  depends_on = [aws_security_group.test-ec2-sg]
-  ami             =   var.ubuntu_ami
-  instance_type   = var.instance_type
-  subnet_id       = aws_subnet.public-1.id
-  security_groups = [aws_security_group.test-ec2-sg.id]
-  key_name        = aws_key_pair.my_key.key_name
-
-  tags = {
-    Name = "${var.project}-test-vpc"
-  }
-
-  associate_public_ip_address = true
 }
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,6 @@
+variable "ubuntu_ami" {}
+variable "instance_type" {}
+variable "project" {}
+variable "subnet_id" {}
+variable "public_key" {}
+variable "vpc_id" {}
